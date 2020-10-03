353a5b187afa3412115ad99eb6559e777288052d
Mugisha Thierry <thierrymugis@gmail.com>
2020-10-03 10:48:13 +0200

    Added java folder

---
+++ java/java.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.work1').hover(function(){
+        $('.overlay1').toggle();
+    })
+
+    $('.work2').hover(function(){
+        $('.overlay2').toggle();
+    })
+})
+
