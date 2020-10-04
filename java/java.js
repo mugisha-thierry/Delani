73eac70c2a8fdc0459037392b221cbc5fdcbc80c
Mugisha Thierry <thierrymugis@gmail.com>
2020-10-04 02:48:53 +0200

    Added spacing

--- java/java.js
+++ java/java.js
@@ -1,60 +1,60 @@
-$(document).ready(function(){
-$('.work1').hover(function(){
+$(document).ready(function () {
+    $('.work1').hover(function () {
         $('.overlay1').toggle();
     })
 
-    $('.work2').hover(function(){
+    $('.work2').hover(function () {
         $('.overlay2').toggle();
     })
-    $('.work3').hover(function(){
+    $('.work3').hover(function () {
         $('.overlay3').toggle();
     })
-    $('.work4').hover(function(){
+    $('.work4').hover(function () {
         $('.overlay4').toggle();
     })
-    $('.work5').hover(function(){
+    $('.work5').hover(function () {
         $('.overlay5').toggle();
     })
-    $('.work6').hover(function(){
+    $('.work6').hover(function () {
         $('.overlay6').toggle();
     })
-    $('.work7').hover(function(){
+    $('.work7').hover(function () {
         $('.overlay7').toggle();
     })
-    $('.work8').hover(function(){
+    $('.work8').hover(function () {
         $('.overlay8').toggle();
     })
-    $('.design').click(function(){
+    $('.design').click(function () {
         $('.design-text').show()
         $('.design').hide()
-   
+
     })
-    $('.design-text').click(function(){
-       $('.design').show()
-       $('.design-text').hide()  
-   });
+    $('.design-text').click(function () {
+        $('.design').show()
+        $('.design-text').hide()
+    });
 
-   $('.development').click(function(){
-    $('.development-text').show()
-    $('.development').hide()
+    $('.development').click(function () {
+        $('.development-text').show()
+        $('.development').hide()
 
+    })
+    $('.development-text').click(function () {
+        $('.development').show()
+        $('.development-text').hide()
+    });
+
+    $('.product').click(function () {
+        $('.product-text').show()
+        $('.product').hide()
+
+    })
+    $('.product-text').click(function () {
+        $('.product').show()
+        $('.product-text').hide()
+    });
 })
-$('.development-text').click(function(){
-   $('.development').show()
-   $('.development-text').hide()  
-});
 
-$('.product').click(function(){
-    $('.product-text').show()
-    $('.product').hide()
-
-})
-$('.product-text').click(function(){
-   $('.product').show()
-   $('.product-text').hide()  
-});
-})
-
-function calc(){
+function calc() {
     alert("we have received your message. Thank you fro reaching out to us.")
 }
